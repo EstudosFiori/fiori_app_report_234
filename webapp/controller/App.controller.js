61ebaa621c64fc379231bb3d5dfbafc9a90d59e0
Wesley Felix <wesleyfelix.developer@gmail.com>
2024-03-04 23:47:44 +0000

    Inicio

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("br.com.gestao.fioriappreport234.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
