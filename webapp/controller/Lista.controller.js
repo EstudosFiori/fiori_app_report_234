7c863c8d34045529726bd5260278f10a09735543
Wesley Felix <wesleyfelix.developer@gmail.com>
2024-03-05 19:14:38 +0000

    Commit - Filter Category

--- webapp/controller/Lista.controller.js
+++ webapp/controller/Lista.controller.js
@@ -2,12 +2,13 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "br/com/gestao/fioriappreport234/util/Formatter"            
+    "br/com/gestao/fioriappreport234/util/Formatter",
+    "sap/ui/core/Fragment"            
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Filter, FilterOperator, Formatter) {
+    function (Controller, Filter, FilterOperator,Formatter, Fragment) {
         "use strict";
 
         return Controller.extend("br.com.gestao.fioriappreport234.controller.Lista", {
@@ -15,7 +16,7 @@ sap.ui.define([
             objFormatter: Formatter,
 
             onInit: function () {
-              //debugger;
+            //debugger;
             //    var oConfiguration = sap.ui.getCore().getConfiguration();
             //    oConfiguration.setFormatLocale("en_US");
 
@@ -23,14 +24,22 @@ sap.ui.define([
             onSearch: function(){
                 var oProdutoInput       = this.getView().byId("productIdIput");
                 var oProdutoNameInput   = this.getView().byId("productNameIput");
+                var oProdutoCategoryInput   = this.getView().byId("productCategoryIput");
  
-                var oFilter = new Filter({
-                    filters: [
-                        new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()),
-                        new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue())
-                    ],
-                    and: true
-                });
+                var objFilter = { filters: [], and: true };
+                objFilter.filters.push(new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()));
+                objFilter.filters.push(new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue()));
+                objFilter.filters.push(new Filter("Category", FilterOperator.Contains, oProdutoCategoryInput.getValue()));
+
+                var oFilter = new Filter(objFilter);
+
+                // var oFilter = new Filter({
+                //     filters: [
+                //         new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()),
+                //         new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue())
+                //     ],
+                //     and: true
+                // });
  
                 var oTable = this.getView().byId("tableProdutos");
                 var oBinding = oTable.getBinding("items");
@@ -48,7 +57,62 @@ sap.ui.define([
                 oRouter.navTo("Detalhes",{
                     productId: oProductId
                 });
-            }
+            },
+
+            onCategoria: function (oEvent){
+                debugger;
+                this._oInput = oEvent.getSource().getId();
+                var oView = this.getView();
+
+                if (!this._CategoriaSearchHelp) {
+                    this._CategoriaSearchHelp = Fragment.load({
+                        id: oView.getId(),
+                        name: "br.com.gestao.fioriappreport234.frags.SH_Categorias",
+                        controller: this
+                    }).then(function(oDialog){
+                        oView.addDependent(oDialog);
+                        return oDialog;                                                
+                    });
+                }
+                this._CategoriaSearchHelp.then(function(oDialog){
+                    oDialog.getBinding("items").filter([]);
+                    oDialog.open();                        
+                })                
+            },            
+
+            onValueHelpSearch: function(oEvent){
+                var sValue = oEvent.getParameter("value");
+
+                // Opção 1 - Crio um único objeto filtro:
+                // var oFilter = new Filter("Description", FilterOperator.Contains, sValue);
+                // oEvent.getSource().getBinding("items").filter([oFilter]);
+
+                // Opção 2 - Podemos criar um objeto (dinamico) onde adiciono várias propriedades:
+                var objFilter = { filters: [], and: false };
+                objFilter.filters.push(new Filter("Description", FilterOperator.Contains, sValue));
+                objFilter.filters.push(new Filter("Category", FilterOperator.Contains, sValue));
+
+                var oFilter = new Filter(objFilter);
+                oEvent.getSource().getBinding("items").filter(oFilter);
+            },
+
+            onValueHelpClose: function (oEvent) {
+                var oSelectedItem = oEvent.getParameter("selectedItem");
+                var oInput = null;
+
+                if (this.byId(this._oInput)) {
+                    oInput = this.byId(this._oInput);
+                } else {
+                    oInput = sap.ui.getCore().byId(this._oInput)                    
+                }
+
+                if (!oSelectedItem) {
+                    oInput.resetProperty("value");
+                    return;
+                }
+
+                oInput.setValue(oSelectedItem.getTitle());
+            }            
         });
     });
  
